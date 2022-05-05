73e4408ce27d1fcfa645cf305196c82c185f9e83
Fast-toe <fausto2017@gmail.com>
2022-05-04 20:17:46 -0500

    clean up of current code

--- Major Maker/Major Maker/Source.cpp
+++ Major Maker/Major Maker/Source.cpp
@@ -1,31 +1,35 @@
 #include <iostream> 
 using namespace std;
 
-void majorSelection() 
+void majorSelection()
 {
 
-	std::cout << "Insert Name and Major\n";
-	std::cout << "1.Welding Technology, Associate of Applied Science\n2.General Studies, Associate of Arts\n"
-		<< "3.Accounting, Associate of Applied Science\n4.Real Estate, Associate of Applied Science\n"
-		<< "5.Eye Care Technology, Associate of Applied Science" << endl;
-	
+	cout << "Insert First Name and Major\n"
+	     << "1.Welding Technology, Associate of Applied Science\n2.General Studies, Associate of Arts\n"
+	 	 << "3.Accounting, Associate of Applied Science\n4.Real Estate, Associate of Applied Science\n"
+ 	     << "5.Eye Care Technology, Associate of Applied Science\n\n";
+
 }
-
-void menu() 
+void classtimes() // to reduce line of code 
 {
-	int choice;
-
+	cout << "Face to Face\n"
+		 << "6:30am  7:40am  8:50am  10:00am  11:10am  12:20pm  1:30pm  2:40pm  3:50pm  5:00pm\n"
+		 << "Online\n"
+		 << "10:00am  11:10am  12:20pm  1:30pm  2:40pm  3:50pm  5:00pm  6:10pm  7:20pm  8:30pm\n\n";
+}
+void menu()
+{
 	bool running = true;
 	while (running) //menu
 	{
 		// 0 will start the program, 1 will end the program
 		cout << "0. Get Started\n" << "1. Quit\n";
 		int choice;
-		std::cin >> choice; 
+		cin >> choice;
 
 		string userName; //store name and major selection
 		int majorNum;
-				
+
 		switch (choice)
 		{
 		case 0:
@@ -36,37 +40,45 @@ void menu()
 			switch (majorNum)
 			{
 			case 1:
-				cout << userName << " You have pick Welding Technology, Associate of Applied Science" << endl;
-				running = false;
+				cout << "\n\n" << userName << "\nYou have pick Welding Technology, Associate of Applied Science\n"
+					 << "The classes you must take are\nIntroduction to Blueprint Reading,\n"
+					 << "Fundamentals of Oxy - Fuel Welding and Cutting,\nIntroduction to Gas Tungsten Arc Welding(GTAW),\n"
+					 << " and Advanced Shielded Metal Arc Welding(SMAW)\n";
+				classtimes();
 				break;
 			case 2:
-				cout << userName << " You have pick General Studies, Associate of Arts" << endl;
-				running = false;
+				cout << "\n\n" << userName << "\nYou have pick General Studies, Associate of Arts "
+					<< "The classes are you must take are\n Composition I,\n"
+					<< "College Algebra,\nBiology for Science Majors,\n and  United States History\n";
+				classtimes();
 				break;
 			case 3:
-				cout << userName << " You have pick Accounting, Associate of Applied Science" << endl;
-				running = false;
+				cout << "\n\n" << userName << "\nYou have pick Accounting, Associate of Applied Science\n"
+					 << "The classes you must take are\nPrinciples of Financial Accounting,\nBusiness Principles,\n"
+					 << "Payroll and Business Tax Accounting,\n and Introduction to Computerized Accounting\n";
+				classtimes();
 				break;
 			case 4:
-				cout << userName << " You have pick Real Estate, Associate of Applied Science" << endl;
-				running = false;
+				cout << "\n\n" << userName << "\nYou have pick Real Estate, Associate of Applied Science\n"
+					 << "The classes you must take are\nPrinciples of Real Estate,\nLaw of Contracts,\n"
+					 << "Real Estate Finance,\n and Law of Agency\n";
+				classtimes();
 				break;
 			case 5:
-				cout << userName << " You have pick Eye Care Technology, Associate of Applied Science" << endl;
-				running = false;
+				cout << "\n\n" << userName << "\nYou have pick Eye Care Technology, Associate of Applied Science\n"
+					 << "The classes you must take are\nVisual System,\nOphthalmic Techniques,\n"
+					 << "Introduction to Eye Care Professions,\n and Vision Care Office Procedures\n";
+				classtimes();
 				break;
 			}
-			
-			running = false;
 			break;
 		case 1:
-			std::cout << "Thank You\n";
+			cout << "Thank You\n";
 			running = false;
 			break;
 		}
 	}
 }
-
 int main()
 {
 	menu();
